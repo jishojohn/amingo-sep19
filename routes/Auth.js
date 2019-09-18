484b6d578ea75b11f9245f6f2b69a78b2213a94d
Developer <dev@Minhajs-MacBook-Pro.local>
2019-09-18 19:18:34 +0400

    Adding login route

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const keys = require('../config/keys');
 
 const router = express.Router();
 
@@ -41,6 +43,54 @@ router.post('/register', (req, res) => {
             }
         })
 });
+
+/**
+ * Post route for login
+ * 
+ * @name POST /auth/login
+ *
+ * @param {string} email - email of the user
+ * @param {string} password - password of the user
+ */
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({email: email}).then(user => {
+        
+        if (user) {
+            
+            bcrypt.compare(password, user.password).then(isMatch => {
+                if (isMatch) {
+
+                    const payload = { id: user.id, name: user.name, email: user.email };
+
+                    // Sign Token
+                    jwt.sign(
+                        payload,
+                        keys.secret,
+                        (err, token) => {
+                            res.json({
+                                success: true,
+                                token: token,
+                                name: user.name
+                            });
+                        }
+                    );
+
+                } else {
+                    res.status(400).json({message: "Wrong password entered!!"})
+                }
+            });
+
+        } else {
+            res.status(400).json({message: "User not found"});
+        }
+    }).catch( err=> {
+        res.status(400).json({message: "User not found"});
+    })
+});
+
 
 
 module.exports = router;
